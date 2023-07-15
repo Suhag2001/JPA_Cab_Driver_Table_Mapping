47ca57ccc9f91771e07525267c29e2544b7d05bf
Suhag Mohinkar <113937289+Suhag2001@users.noreply.github.com>
2023-07-15 01:46:16 -0700

    commit

--- src/main/java/com/bootcoding/JPACabDriverTableMaping/repo/CabDriverRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bootcoding.JPACabDriverTableMaping.repo;
-
-import com.bootcoding.JPACabDriverTableMaping.entity.CabDriver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CabDriverRepo extends JpaRepository<CabDriver,Integer> {
-}
--- src/main/java/com/bootcoding/JPACabDriverTableMaping/repo/RatingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bootcoding.JPACabDriverTableMaping.repo;
-
-import com.bootcoding.JPACabDriverTableMaping.entity.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RatingRepo extends JpaRepository<Rating,Integer> {
-}
